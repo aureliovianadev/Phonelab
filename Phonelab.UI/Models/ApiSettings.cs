1106c655f8b31dd569e0a977a3331fca8f42379f
aureliovianadev <aureliovianadev@gmail.com>
2025-11-18 20:33:28 -0300

    Início do MVC

---
+++ Phonelab.UI/Models/ApiSettings.cs
@@ -0,0 +1,9 @@
+namespace Phonelab.UI.Models;
+
+public class ApiSettings
+{
+    public string BaseUrl { get; set; } = string.Empty;
+    public int TimeoutSeconds { get; set; } = 30;
+}
+
+
---
+++ Phonelab.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Phonelab.UI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
